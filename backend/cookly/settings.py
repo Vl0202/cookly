83e8c53829aa918e6728151defb34c70564de4bd
Владимир Ли <vovalee@MBP-Vladimir.z24.guap.ru>
2025-12-02 05:55:37 +0300

    1

--- backend/cookly/settings.py
+++ backend/cookly/settings.py
@@ -1,20 +1,23 @@
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('SECRET_KEY', 'django')
+
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
-SECRET_KEY = 'django-insecure-lat5@4*et0!f-a#v1gq(pmfm8lv(y)-+_leh4_b!0rm2a^0#4y'
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 AUTH_USER_MODEL = 'recipes.UserProfile'
 
 INSTALLED_APPS = [
-   'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -59,14 +62,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cookly.wsgi.application'
 
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -85,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
@@ -96,12 +101,16 @@ USE_L10N = True
 USE_TZ = True
 
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
+
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -117,6 +126,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
+
+
 DJOSER = {
     'LOGIN_FIELD': 'email',
     'HIDE_USERS': False,
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,159 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import mark_safe
 
-# Register your models here.
+from .models import (Favorite, Ingredient, IngredientAmount, Recipe,
+                     ShoppingCart, Subscribe, Tag, UserProfile)
+
+
+class CountRecipesMixin:
+    list_display = ('recipe_count',)
+
+    @admin.display(
+        description='Рецепты',
+        ordering='recipes__count'
+    )
+    def recipe_count(self, obj):
+        return obj.recipes.count()
+
+
+class SubscribeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'follower', 'following')
+    list_filter = ('following', 'follower')
+    search_fields = (
+        'follower__username',
+        'following__username'
+    )
+
+
+class UserProfileAdmin(CountRecipesMixin, UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительно', {
+            'fields': (
+                'avatar',
+            )
+        }),
+    )
+    list_display = (
+        'username',
+        'email',
+        'get_full_name',
+        'avatar_tag',
+        'get_subscriptions_count',
+        'get_subscribers_count',
+        *CountRecipesMixin.list_display
+    )
+
+    @admin.display(description='Полное имя')
+    def get_full_name(self, user):
+        return f"{user.first_name} {user.last_name}".strip()
+
+    @admin.display(description='Аватар')
+    def avatar_tag(self, user):
+        if user.avatar:
+            return mark_safe(
+                f'<img src="{user.avatar.url}" width="50" height="50">')
+        return "Нет аватара"
+    avatar_tag.short_description = 'Аватар'
+
+    @admin.display(description='Подписки')
+    def get_subscriptions_count(self, obj):
+        return obj.authors.count()
+
+    @admin.display(description='Подписчики')
+    def get_subscribers_count(self, obj):
+        return obj.followers.count()
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related(
+            'authors', 'followers'
+        )
+
+
+class IngredientAdmin(CountRecipesMixin, admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'measurement_unit', *CountRecipesMixin.list_display
+    )
+    search_fields = ('name', 'measurement_unit')
+    list_filter = ('measurement_unit',)
+
+
+class IngredientAmountInline(admin.TabularInline):
+    model = IngredientAmount
+    extra = 1
+    fields = ('ingredient', 'amount')
+    autocomplete_fields = ['ingredient']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [IngredientAmountInline]
+    list_display = (
+        'id', 'name', 'get_author_username',
+        'cooking_time', 'get_ingredients',
+        'get_tags', 'image_tag', 'count_favorites'
+    )
+    list_filter = ('tags', 'author__username',)
+    search_fields = ('name__icontains', 'author__username__icontains')
+
+    @admin.display(description='Изображение')
+    def image_tag(self, recipe):
+        if recipe.image:
+            return mark_safe(
+                f'<img src="{recipe.image.url}" width="50" height="50" />')
+        return "Нет изображения"
+
+    @admin.display(
+        description='логин')
+    def get_author_username(self, obj):
+        return obj.author.username
+
+    @admin.display(
+        description='Ингредиенты',
+        ordering='ingredients__name'
+    )
+    def get_ingredients(self, recipe):
+        ingredients_list = [
+            f'- {ing.ingredient.name} '
+            f'({ing.amount} {ing.ingredient.measurement_unit})'
+            for ing in recipe.recipe_amounts.select_related('ingredient').all()
+        ]
+        return mark_safe('<br>'.join(ingredients_list))
+
+    @admin.display(
+        description='Теги',
+        ordering='tags__name'
+    )
+    def get_tags(self, obj):
+        return ', '.join(tag.name for tag in obj.tags.all())
+
+    @admin.display(
+        description='Лайки',
+        ordering='favorites__count'
+    )
+    def count_favorites(self, recipe):
+        return recipe.favorites.count()
+
+
+class TagAdmin(CountRecipesMixin, admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', *CountRecipesMixin.list_display)
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
+    list_filter = ('user', 'recipe')
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
+    list_filter = ('user', 'recipe')
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Subscribe, SubscribeAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
